ad5b0e1914b622985b1ebff119a8fa82fa3ef026
hszillat <holger.szillat@posteo.de>
2022-01-09 13:25:18 +0100

    Initial commit.

---
+++ src/test/java/de/szillat/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package de.szillat.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
