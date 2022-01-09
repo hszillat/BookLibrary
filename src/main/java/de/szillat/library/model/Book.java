fbe55f3823d1ca45c77f6418ab35e23607d15a9c
hszillat <holger.szillat@posteo.de>
2022-01-09 14:25:25 +0100

    Using SQLite for persistence.

--- src/main/java/de/szillat/library/model/Book.java
+++ src/main/java/de/szillat/library/model/Book.java
@@ -1,18 +1,26 @@
 package de.szillat.library.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Optional;
 
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     public Long id;
+
     public String title;
-    public Optional<String> originalTitle;
+    public String originalTitle;
     public String isbn;
     public int publishedYear;
 
-    private Book() {
+    protected Book() {
         id = null;
-        title = isbn = "";
-        originalTitle = Optional.empty();
+        title = isbn = null;
+        originalTitle = null;
         publishedYear = 0;
     }
 
@@ -21,7 +29,7 @@ public class Book {
         return "Book{" +
                 "id=" + id +
                 ", title='" + title + '\'' +
-                ", originalTitle='" + originalTitle.orElse("") + '\'' +
+                ", originalTitle='" + originalTitle + '\'' +
                 ", isbn='" + isbn + '\'' +
                 ", publishedYear=" + publishedYear +
                 '}';
