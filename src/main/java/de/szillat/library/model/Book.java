159ea84a1ebf80e29578490605d598606298ea6b
hszillat <holger.szillat@posteo.de>
2022-01-11 15:58:24 +0100

    Adding some validity checks to the Book-model.

--- src/main/java/de/szillat/library/model/Book.java
+++ src/main/java/de/szillat/library/model/Book.java
@@ -1,5 +1,8 @@
 package de.szillat.library.model;
 
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.lang.NonNull;
 
 import javax.persistence.*;
@@ -8,17 +11,19 @@ import java.util.Optional;
 @Entity
 @Table(name = "books", indexes = @Index(columnList = "isbn"))
 public class Book {
+    private static final Logger _log = LoggerFactory.getLogger(Book.class);
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     public Long id;
 
     @NonNull
-    public String title;
+    private String title;
 
     public String originalTitle;
 
     @NonNull
-    public String isbn;
+    private String isbn;
 
     public int publishedYear;
 
@@ -27,6 +32,30 @@ public class Book {
         title = isbn = null;
         originalTitle = null;
         publishedYear = 0;
+    }
+
+    @NonNull
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(@NonNull String title) {
+        if (StringUtils.isNotBlank(title))
+            this.title = title;
+
+        _log.debug("Given title is empty!");
+    }
+
+    @NonNull
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(@NonNull String isbn) {
+        if (StringUtils.isNotBlank(isbn))
+            this.isbn = isbn;
+
+        _log.debug("Given isbn is empty!");
     }
 
     @Override
