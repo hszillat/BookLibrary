c0265ad7d84480a14ccaa813bb5ff0ecbf22d567
hszillat <holger.szillat@posteo.de>
2022-01-24 15:53:27 +0100

    Added Login-Page before using the web-application.

--- src/main/java/de/szillat/library/WebSecurityConfig.java
+++ src/main/java/de/szillat/library/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package de.szillat.library;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,6 +17,8 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private static final Logger _log = LoggerFactory.getLogger(WebSecurityConfig.class);
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
